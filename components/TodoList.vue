d32eb0aad5f64381ac7483be43899b187e71d1ec
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-08-27 16:01:39 +0900

    feat: TodoListコンポーネントを作成

---
+++ components/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Todo List
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        single-line
+        hide-details
+      >
+      </v-text-field>
+      <v-data-table
+        :headers="headers"
+        :items="todos"
+        :search="search"
+      >
+      </v-data-table>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          title: "test",
+          username: "太郎"
+        }
+      ],
+      search: "",
+      headers: [
+        {
+          text: "タイトル",
+          align: "left",
+          sortable: false,
+          value: "title"
+        },
+        { 
+          text: "ユーザー名", 
+          value: "username"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
